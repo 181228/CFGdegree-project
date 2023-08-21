568fffe341403287621d67683f7db6df10b01fff
Agnieszka Thiel <115164339+AgInsideOut@users.noreply.github.com>
2023-08-21 17:37:23 +0200

    vulnerability-issues-solved

--- readswap-app/src/utils/apiConfig.js
+++
@@ -1,4 +0,0 @@
-// KEY AND HOST FOR BOOK FINDER API
-
-export const BOOK_FINDER_API_KEY = '0dc4d8763emsh1ad10d22069b0d1p12dff8jsnb164eb9263ac';
-export const BOOK_FINDER_API_HOST = 'book-finder1.p.rapidapi.com';
