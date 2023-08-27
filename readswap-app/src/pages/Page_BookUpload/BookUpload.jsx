ebbaa22801fdd43adb62bfa639b7a68493b243e8
Agnieszka Thiel <115164339+AgInsideOut@users.noreply.github.com>
2023-08-27 07:55:03 +0200

    book-upload-user-authentication

--- readswap-app/src/pages/Page_BookUpload/BookUpload.jsx
+++ readswap-app/src/pages/Page_BookUpload/BookUpload.jsx
@@ -13,43 +13,59 @@ const BookUploadForm = () => {
         image: '',
     });
 
+    const [error, setError] = useState(null);
+    const [tokenMissing, setTokenMissing] = useState(false);
+
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
+            ...prevData,
+            [name]: value,
         }));
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        
+
+        // Check if token is missing
+        if (tokenMissing) {
+            setError('Token is missing. Please log in.');
+            return;
+        }
+
+        // Check if username is provided
+        if (!formData.username) {
+            setError('Please enter a username before posting a book.');
+            return;
+        }
+
         try {
             // GET USER_ID BASED ON USERNAME
             const username = formData.username;
-            
+
             const userIdResponse = await fetch(`http://localhost:3000/api/user-id/${username}`);
-    
+
             if (!userIdResponse.ok) {
                 throw new Error('User not found');
             }
-    
+
             const userIdData = await userIdResponse.json();
-    
+
             // Check if all required fields are filled
-            const requiredFields = ['title', 'author', 'genre', 'condition', 'tradeFor', 'price', 'image'];
+            const requiredFields = ['title', 'author', 'genre', 'condition', 'trade_for', 'price', 'image'];
             const missingFields = requiredFields.filter(field => !formData[field]);
-    
+
             if (missingFields.length > 0) {
-                throw new Error('Please fill in all required fields');
+                setError('Please fill in all required fields');
+                return;
             }
-    
+
             // SET USER_ID IN FORMDATA BASED ON RESULT OF A QUERY
             setFormData((prevData) => ({
                 ...prevData,
                 user_id: userIdData.id,
             }));
-    
+
             const response = await fetch('http://localhost:3000/api/books', {
                 method: 'POST',
                 headers: {
@@ -57,7 +73,7 @@ const BookUploadForm = () => {
                 },
                 body: JSON.stringify(formData),
             });
-    
+
             if (response.ok) {
                 // RESET FORM FIELDS
                 setFormData({
@@ -70,14 +86,23 @@ const BookUploadForm = () => {
                     price: '',
                     image: '',
                 });
+                // RESET ERRORS
+                setError(null);
+                setTokenMissing(false);
                 // DISPLAY SUCCESS MESSAGE
                 window.alert('Book posted!');
             } else {
                 // PLACE FOR HANDLING ERRORS
-                throw new Error('Error posting book');
+                if (response.status === 401) {
+                    setError('Unauthorized. Please log in.');
+                } else if (response.status === 400) {
+                    setError('Bad request. Please check your data.');
+                } else {
+                    setError('Error posting book');
+                }
             }
         } catch (error) {
-            console.error('Error during form submission:', error);
+            setError('Error during form submission');
         }
     };    
     
@@ -89,8 +114,10 @@ const BookUploadForm = () => {
         <br />
         <div className="upload-details">
         <h2 className="upload-h2">Upload a Book</h2>
+        {error && <p className="error-message">{error}</p>}
+        {tokenMissing && <p className="error-message">Token is missing. Please log in.</p>}
         <form onSubmit={handleSubmit}>
-            {/* Owner's Email */}
+            {/* Owner's Username */}
             <label className="upload-label">Username:</label>
             <input
                 className="upload-input"
@@ -150,8 +177,8 @@ const BookUploadForm = () => {
             <input
                 className="upload-input"
                 type="text"
-                name="tradeFor"
-                value={formData.tradeFor}
+                name="trade_for"
+                value={formData.trade_for}
                 onChange={handleInputChange}
                 required
             />
